64eb7b22c190faa9306036e952c9727fad831eab
Andrey-O-Kotlyarov <weiss14@yandex.ru>
2026-03-10 19:08:28 +0300

    добавление созданных уроков в базу

--- src/main/java/testgroup/service/LessonService.java
+++ src/main/java/testgroup/service/LessonService.java
@@ -1,5 +1,12 @@
 package testgroup.service;
 
-public interface LessonService {
+import java.util.Optional; 
+import testgroup.model.Lesson;
+import testgroup.model.User;
 
+public interface LessonService { 
+
+    public Long createLesson(Long number, String title, String content, User user); 
+    public Optional<Lesson> getLatestLessonForUser(User user); 
+    public Optional<Lesson> getLatestLessonForUserId(Long userId); 
 }
--- src/main/java/testgroup/service/LessonServiceImpl.java
+++ src/main/java/testgroup/service/LessonServiceImpl.java
@@ -1,8 +1,40 @@
 package testgroup.service;
 
-import org.springframework.stereotype.Service;
+import java.util.Optional; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service; 
+import testgroup.dao.LessonRepository;
+import testgroup.model.Lesson;
+import testgroup.model.User;
 
 @Service
-public class LessonServiceImpl implements LessonService{
+public class LessonServiceImpl implements LessonService { 
 
-}
+    @Autowired
+    private LessonRepository lessonRepository; 
+
+
+    @Override
+    public Long createLesson(Long number, String title, String content, User user) { 
+        Lesson lesson = new Lesson(); 
+        lesson.setNumber(number); 
+        lesson.setTitle(title); 
+        lesson.setContent(content);  
+        lesson.setUser(user);        
+        Lesson savedLesson = lessonRepository.save(lesson); 
+        Long id = savedLesson.getId(); 
+        return id; 
+    }
+
+
+    @Override
+    public Optional<Lesson> getLatestLessonForUser(User user) {
+        return lessonRepository.findTopByUserOrderByNumberDesc(user);
+    }
+
+
+    @Override
+    public Optional<Lesson> getLatestLessonForUserId(Long userId) {
+        return lessonRepository.findTopByUser_IdOrderByNumberDesc(userId);
+    } 
+} 
